b062d3be3adfbfd806538b7929767c6bd5c9b00a
Andre Wonasue <awonasue@terpmail.umd.edu>
2024-11-25 18:08:50 -0500

    First commit/start

---
+++ Dice_game.py
@@ -0,0 +1,12 @@
+#Tuple out game
+
+#Current Rules: Score the most points/ Set point specified. Players take turn rolling
+#Each turn, the active player rolls three dice.
+
+
+#For random dice rolls
+import random
+
+#Function for rolling a 6 sided die
+
+
