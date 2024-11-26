7ac098ef5a5afff62f796018f83d29ef58a1ac3b
Andre Wonasue <awonasue@terpmail.umd.edu>
2024-11-26 17:47:41 -0500

    Rules clarifications.

--- Dice_game.py
+++ Dice_game.py
@@ -8,11 +8,12 @@ def short_rule():
     Tupple out game.
         Rules: Let luck be on your side
         1. Maximum player is 3 but 1-3 can play. Each player enters their name.
-        2.Players set a winning score. highest score at the end of 3 turn wins
+        2.Players set a winning score. highest score at the end wins
         3.Rolling two of the same number allows you to reroll if you want.
         4.Tuple [4,4,4] out mean you earn 0 points
         6.If 3 die are unique [1,2,3], player can reroll 3 die once or choose not to
         7. If you choose to reroll 3 "unique" number and get [3,5,3]. You cant reroll
+        8. Enter 'y' or 'no' when prompted. Failure defaults to a no reroll
           """)
 
 #Function for rolling a 6 sided die
